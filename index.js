42d16b1d0376c2bcff0ddaa13b82bc5a5be2141c
AngelaFig <angelafigueroa596@gmail.com>
2023-08-02 11:38:02 -0400

    added node-mod and package-lock to gitignore, created init app function in index, created a write README function in index, and connected generate markdownd.js to index

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // TODO: Include packages needed for this application
 const fs = require('fs/promises');
+const inquirer= require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data).then((err,data)=>{
+        if(err) console.log(err)
+        console.log("README Generated Successfully")
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Init Function Executed")
+    inquirer.prompt(questions).then(data=> {
+        let readme_html =generateMarkdown(data)
+        writeToFile("demo.md", readme_html)
+    })
+}
 
 // Function call to initialize app
-init();
+// init();
